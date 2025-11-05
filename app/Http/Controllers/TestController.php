1d4a0f1fcc547b11627ed16930251c1edc06c03e
mostdream <mostdream@naver.com>
2025-11-05 17:46:08 +0900

    test commit & push 11

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Http;
+use PragmaRX\Google2FA\Google2FA;
+use Carbon\Carbon;
+
+class TestController extends Controller
+{
+   public function index()
+    {
+
+    }
+}
