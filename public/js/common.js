f10347725115845059e9cf4e41d1fa939befee42
hoho3028 <hoho.atn@gmail.com>
2025-11-27 16:53:56 +0900

    loginUI변경

--- public/js/common.js
+++ public/js/common.js
@@ -261,3 +261,21 @@ $(document).ready(function() {
         $(this).modal('show');
     });
 });
+
+
+//login box hide > show
+const introVideo = document.getElementsByClassName("bg-video");
+const introWrap = document.querySelector(".login-wrap");
+const loginWrap = document.querySelector(".login-box");
+const loginlogo = document.querySelector(".loginlogo");
+
+setTimeout(() =>{
+    loginlogo.classList.add("show");
+}, 500);
+setTimeout(() =>{
+    loginlogo.classList.remove("show");
+    loginlogo.classList.add("hide");
+}, 2500);
+setTimeout(() =>{
+    loginWrap.classList.add("show");
+}, 3000);
