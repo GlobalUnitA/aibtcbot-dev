b8938c8c983dfca9a8a953400956c02022274668
hoho3028 <hoho.atn@gmail.com>
2025-11-28 11:43:44 +0900

    loginscript에러수정

--- public/js/auth/login.js
+++ public/js/auth/login.js
@@ -3,3 +3,20 @@ $(document).ready(function() {
          window.location.href = $(this).val();
     });
 });
+
+//login box hide > show
+const introVideo = document.getElementsByClassName("bg-video");
+const introWrap = document.querySelector(".login-wrap");
+const loginWrap = document.querySelector(".login-box");
+const loginlogo = document.querySelector(".loginlogo");
+
+setTimeout(() =>{
+    loginlogo.classList.add("show");
+}, 500);
+setTimeout(() =>{
+    loginlogo.classList.remove("show");
+    loginlogo.classList.add("hide");
+}, 2500);
+setTimeout(() =>{
+    loginWrap.classList.add("show");
+}, 3000);
--- public/js/common.js
+++ public/js/common.js
@@ -263,19 +263,3 @@ $(document).ready(function() {
 });
 
 
-//login box hide > show
-const introVideo = document.getElementsByClassName("bg-video");
-const introWrap = document.querySelector(".login-wrap");
-const loginWrap = document.querySelector(".login-box");
-const loginlogo = document.querySelector(".loginlogo");
-
-setTimeout(() =>{
-    loginlogo.classList.add("show");
-}, 500);
-setTimeout(() =>{
-    loginlogo.classList.remove("show");
-    loginlogo.classList.add("hide");
-}, 2500);
-setTimeout(() =>{
-    loginWrap.classList.add("show");
-}, 3000);
