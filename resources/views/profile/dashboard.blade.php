b06e13643a6fae926fdfc5530b22168ed828edc2
hoho3028 <hoho.atn@gmail.com>
2025-11-28 16:54:33 +0900

    dashboardUI정렬추가2

--- resources/views/profile/dashboard.blade.php
+++ resources/views/profile/dashboard.blade.php
@@ -69,9 +69,9 @@
                 </p>
                 @foreach ($data['avatars'] as $avatar)
                 <div class="p-4 rounded bg-light text-body mb-4">
-                    <div class="d-flex justify-content-between mb-3">
+                    <div class="d-flex justify-content-between mb-2">
                         <a href="{{ route('avatar.view', ['id' => $avatar->id]) }}">
-                            <p class="text-body fs-4 m-0" style="font-weight:bold;color:#6976A2 !important;font-weight: bold;display: inline-block;vertical-align: middle;">{{ __('user.avatar') }} {{ $loop->iteration }}</p>
+                            <p class="text-body fs-5 m-0" style="font-weight:bold;color:#6976A2 !important;font-weight: bold;display: inline-block;vertical-align: middle;">{{ __('user.avatar') }} {{ $loop->iteration }}</p>
                             <img src="{{ asset('images/icon/right_icon_g.svg') }}" style="display: inline-block;width: auto;height: 24px;">
                         </a>
                         @if ($avatar->is_active === 'n')
