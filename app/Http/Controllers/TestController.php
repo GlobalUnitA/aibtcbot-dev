5553199b7554d6a960a9f087e31e90d23f324883
aethierTest2 <cjo092783@gmail.com>
2025-12-10 15:04:32 +0900

    출금제한금액 산출 테스트

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -34,10 +34,45 @@ class TestController extends Controller
 
     public function __construct()
     {
-        $this->kakaoApi = new KakaoApi();
+        $this->kakaoApi = new KakaoApi();;
     }
-   public function index($id)
-    {
 
+   public function index()
+    {
+        $user = Auth::user();
+        $member = Member::where('user_id', $user->id)->first();
+        $income = $member->incomes->where('coin_id', '1')->first();
+
+        $product = $income->accumulation->product;
+
+        $base = $product->avatar_target_amount - $product->avatar_cost;
+
+        dump('base : ', $base);
+        $accumulated_profit = $income->transfers()
+            ->where('type', 'referral_bonus')
+            ->where('status', 'completed')
+            ->sum('amount');
+
+        dump('accumulated_profit : ', $accumulated_profit);
+        $accumulated_withdrawn = $income->transfers()
+            ->where('type', 'withdrawal')
+            ->where('status', 'completed')
+            ->sum('amount');
+
+        dump('accumulated_withdrawn : ', $accumulated_withdrawn);
+
+        $int_part = intdiv($accumulated_profit, $product->avatar_target_amount);
+        dump('int_part : ', $int_part);
+
+        $mod_part = $accumulated_profit % $product->avatar_target_amount;
+        dump('mod_part : ', $mod_part);
+
+        $min_part = min($base, $mod_part);
+        dump('min_part : ', $min_part);
+
+        $withdrawable_amount = max(0, ($base * $int_part) + $min_part - $accumulated_withdrawn);
+        dump('withdrawable_amount : ', $withdrawable_amount);
+
+        dd(min($withdrawable_amount, $income->balance));
     }
 }
